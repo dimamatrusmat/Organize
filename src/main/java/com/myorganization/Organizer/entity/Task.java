8a6a4fd9d4a3330d8a2f8826be11cb5fdcc46c4a
Abolior <dimamatveev121@gmail.com>
2023-02-03 13:14:00 +0500

    finish

---
+++ src/main/java/com/myorganization/Organizer/entity/Task.java
@@ -0,0 +1,69 @@
+package com.myorganization.Organizer.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import org.springframework.data.annotation.Id;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+
+@Entity
+public class Task {
+
+    @jakarta.persistence.Id
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private Integer priorityId;
+
+    private String description;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date date;
+
+    public Task() {
+    }
+
+    public Task(Integer priorityId, String description, Date date) {
+        this.id = id;
+        this.priorityId = priorityId;
+        this.description = description;
+
+        this.date = date;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getPriorityId() {
+        return priorityId;
+    }
+
+    public void setPriorityId(Integer priorityId) {
+        this.priorityId = priorityId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+}
